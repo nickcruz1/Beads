23f0b768a96fd582e19d0c83deed86c74c3cbb9b
Nick Cruz <nickcruz920@gmail.com>
2022-07-25 10:52:17 -0400

    Added all bead color functions

---
+++ index.js
@@ -0,0 +1,38 @@
+let white = () => {
+  console.log("White Bead Clicked");
+  let canvas = document.querySelector("#canvas");
+  
+  canvas.style.border = "20px solid black";
+  canvas.style.backgroundColor = "white";
+}
+
+let red = () => {
+  console.log("Red Bead Clicked");
+   let canvas = document.querySelector("#canvas");
+  canvas.style.backgroundColor = "red";
+}
+
+let blue = () => {
+  console.log("Blue Bead Clicked");
+   let canvas = document.querySelector("#canvas");
+  canvas.style.backgroundColor = "blue";
+}
+
+let green = () => {
+  console.log("Green Bead Clicked");
+   let canvas = document.querySelector("#canvas");
+  canvas.style.backgroundColor = "green";
+}
+
+let gray = () => {
+  console.log("Gray Bead Clicked");
+   let canvas = document.querySelector("#canvas");
+  canvas.style.backgroundColor = "gray";
+}
+
+let yellow = () => {
+  console.log("Yellow Bead Clicked");
+   let canvas = document.querySelector("#canvas");
+  
+  canvas.style.backgroundColor = "yellow";
+}
